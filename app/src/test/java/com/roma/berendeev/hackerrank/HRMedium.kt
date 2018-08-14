d4ce77f4e1e5c8296566d583de4e8e7514ba7ab6
Roman Berendeev <roman.berendeev@rentateam.ru>
2018-08-14 18:34:24 +0300

    extraLongFactorials

--- app/src/test/java/com/roma/berendeev/hackerrank/HRMedium.kt
+++ app/src/test/java/com/roma/berendeev/hackerrank/HRMedium.kt
@@ -22,6 +22,7 @@ class HRMedium {
 //        println("subset length " + nonDivisibleSubset(1, arrayOf(1, 2, 3, 4, 5)))
 //        println("subset length " + nonDivisibleSubset(3, arrayOf(1, 7, 2, 4)))
 //        println("subset length " + nonDivisibleSubset(7, arrayOf(278, 576, 496, 727, 410, 124, 338, 149, 209, 702, 282, 718, 771, 575, 436)))
+        extraLongFactorials(25)
     }
 
     private fun climbingLeaderboard(scores: Array<Int>, alice: Array<Int>): Array<Int> {
@@ -76,6 +77,7 @@ class HRMedium {
             fun findMaxFromPair(index: Int): Int {
                 return Math.max(baskets[index], baskets[baskets.size - index])
             }
+
             var result = baskets[0]
             if (k % 2 == 0) {
                 result += baskets[k / 2]
@@ -90,4 +92,50 @@ class HRMedium {
         return countSubsets(baskets)
     }
 
+    private fun extraLongFactorials(n: Int) {
+
+        class MyBigDecimal(initValue: Int = 0) {
+            private val partOverflowValue = 1_000_000_000
+            private val parts = mutableListOf(initValue)
+
+            fun multiply(value: Int) {
+                var overflow = 0L
+                var mult: Long
+
+                val longValue = value.toLong()
+                var partNumber = 0
+                while (partNumber < parts.size) {
+                    val part = parts[partNumber]
+                    mult = part * longValue + overflow
+                    if (mult >= partOverflowValue) {
+                        overflow = mult / partOverflowValue
+                        mult %= partOverflowValue
+                    } else {
+                        overflow = 0
+                    }
+                    parts[partNumber] = mult.toInt()
+                    partNumber++
+                    if (partNumber == parts.size && overflow > 0) {
+                        parts.add(0)
+                    }
+                }
+            }
+
+            override fun toString(): String {
+                return buildString {
+                    parts.mapIndexed { index, value ->
+                        if (index != parts.lastIndex) {
+                            String.format("%09d", value)
+                        } else {
+                            "$value"
+                        }
+                    }.forEach { insert(0, it) }
+                }
+            }
+        }
+
+        val myBigInt = MyBigDecimal(1)
+        (1..n).forEach { myBigInt.multiply(it) }
+        println(myBigInt.toString())
+    }
 }
